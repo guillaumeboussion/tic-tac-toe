8644c22ed194505061ca570da161c97a61fe4837
boussionguillaume <guillaume@hellocse.fr>
2025-12-15 19:03:52 +0100

    feat(ui): add trophy asset constant

--- lib/constants/assets.dart
+++ lib/constants/assets.dart
@@ -3,4 +3,5 @@ class Assets {
   static const String lottieTicTacToe = 'assets/lottie/tic_tac_toe.json';
   static const String lottieLoadingDots = 'assets/lottie/loading_dots.json';
   static const String lottieTrophy = 'assets/lottie/trophy.json';
+  static const String trophyIcon = 'assets/images/trophy_icon.png';
 }
