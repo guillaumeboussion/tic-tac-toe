e114bc0d99ce007c72d1bd73fc28e3bee75cbb95
boussionguillaume <guillaume@hellocse.fr>
2025-12-16 11:11:43 +0100

    refactor(lint): apply dart format

--- lib/generated/intl/messages_fr.dart
+++ lib/generated/intl/messages_fr.dart
@@ -24,70 +24,77 @@ class MessageLookup extends MessageLookupByLibrary {
 
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
-        "ai": MessageLookupByLibrary.simpleMessage("IA"),
-        "ai_turn": MessageLookupByLibrary.simpleMessage("Tour de l\'IA"),
-        "app_title": MessageLookupByLibrary.simpleMessage("Tic Tac Toe"),
-        "attribution":
-            MessageLookupByLibrary.simpleMessage("par Guillaume Boussion ©"),
-        "back_to_home":
-            MessageLookupByLibrary.simpleMessage("Retour à l\'accueil"),
-        "challenge_the_ai":
-            MessageLookupByLibrary.simpleMessage("Défiez l\'IA"),
-        "current_language":
-            MessageLookupByLibrary.simpleMessage("Langue actuelle"),
-        "defeat": MessageLookupByLibrary.simpleMessage("DÉFAITE !"),
-        "draw": MessageLookupByLibrary.simpleMessage("ÉGALITÉ !"),
-        "draws": MessageLookupByLibrary.simpleMessage("Égalités"),
-        "empty_field_error": MessageLookupByLibrary.simpleMessage(
-            "Ce champ ne peut pas être vide"),
-        "english": MessageLookupByLibrary.simpleMessage("Anglais"),
-        "error_loading_history": MessageLookupByLibrary.simpleMessage(
-            "Erreur lors du chargement de l\'historique"),
-        "french": MessageLookupByLibrary.simpleMessage("Français"),
-        "friend": MessageLookupByLibrary.simpleMessage("Ami"),
-        "game_history":
-            MessageLookupByLibrary.simpleMessage("Historique des parties"),
-        "game_time": MessageLookupByLibrary.simpleMessage("Temps de jeu"),
-        "language": MessageLookupByLibrary.simpleMessage("Langue"),
-        "lets_play_together":
-            MessageLookupByLibrary.simpleMessage("Jouons ensemble !"),
-        "loading_experience": MessageLookupByLibrary.simpleMessage(
-            "Chargement de l\'expérience..."),
-        "local_multiplayer":
-            MessageLookupByLibrary.simpleMessage("Multijoueur local"),
-        "losses": MessageLookupByLibrary.simpleMessage("Défaites"),
-        "new_game": MessageLookupByLibrary.simpleMessage("Nouvelle partie"),
-        "no_games_yet": MessageLookupByLibrary.simpleMessage(
-            "Aucune partie jouée. Commencez à jouer pour construire votre historique !"),
-        "page_title_tic_tac_toe":
-            MessageLookupByLibrary.simpleMessage("TIC TAC TOE"),
-        "play_vs_computer":
-            MessageLookupByLibrary.simpleMessage("Jouer contre l\'ordinateur"),
-        "play_vs_friend":
-            MessageLookupByLibrary.simpleMessage("Jouer contre un ami"),
-        "player_1_turn":
-            MessageLookupByLibrary.simpleMessage("Tour du joueur 1"),
-        "player_1_wins":
-            MessageLookupByLibrary.simpleMessage("JOUEUR 1 GAGNE !"),
-        "player_2_turn":
-            MessageLookupByLibrary.simpleMessage("Tour du joueur 2"),
-        "player_2_wins":
-            MessageLookupByLibrary.simpleMessage("JOUEUR 2 GAGNE !"),
-        "previous_games":
-            MessageLookupByLibrary.simpleMessage("Parties précédentes"),
-        "restart": MessageLookupByLibrary.simpleMessage("Recommencer"),
-        "select_game_mode":
-            MessageLookupByLibrary.simpleMessage("Sélectionner le mode de jeu"),
-        "settings": MessageLookupByLibrary.simpleMessage("Paramètres"),
-        "statistics": MessageLookupByLibrary.simpleMessage("Statistiques"),
-        "trophies": MessageLookupByLibrary.simpleMessage("Trophées"),
-        "trophies_earned": m0,
-        "ultimate_edition":
-            MessageLookupByLibrary.simpleMessage("⚡ ÉDITION ULTIME ⚡"),
-        "victory": MessageLookupByLibrary.simpleMessage("VICTOIRE !"),
-        "vs_ai": MessageLookupByLibrary.simpleMessage("VS IA"),
-        "vs_friend": MessageLookupByLibrary.simpleMessage("VS AMI"),
-        "wins": MessageLookupByLibrary.simpleMessage("Victoires"),
-        "your_turn": MessageLookupByLibrary.simpleMessage("Votre tour")
-      };
+    "ai": MessageLookupByLibrary.simpleMessage("IA"),
+    "ai_turn": MessageLookupByLibrary.simpleMessage("Tour de l\'IA"),
+    "app_title": MessageLookupByLibrary.simpleMessage("Tic Tac Toe"),
+    "attribution": MessageLookupByLibrary.simpleMessage(
+      "par Guillaume Boussion ©",
+    ),
+    "back_to_home": MessageLookupByLibrary.simpleMessage("Retour à l\'accueil"),
+    "challenge_the_ai": MessageLookupByLibrary.simpleMessage("Défiez l\'IA"),
+    "current_language": MessageLookupByLibrary.simpleMessage("Langue actuelle"),
+    "defeat": MessageLookupByLibrary.simpleMessage("DÉFAITE !"),
+    "draw": MessageLookupByLibrary.simpleMessage("ÉGALITÉ !"),
+    "draws": MessageLookupByLibrary.simpleMessage("Égalités"),
+    "empty_field_error": MessageLookupByLibrary.simpleMessage(
+      "Ce champ ne peut pas être vide",
+    ),
+    "english": MessageLookupByLibrary.simpleMessage("Anglais"),
+    "error_loading_history": MessageLookupByLibrary.simpleMessage(
+      "Erreur lors du chargement de l\'historique",
+    ),
+    "french": MessageLookupByLibrary.simpleMessage("Français"),
+    "friend": MessageLookupByLibrary.simpleMessage("Ami"),
+    "game_history": MessageLookupByLibrary.simpleMessage(
+      "Historique des parties",
+    ),
+    "game_time": MessageLookupByLibrary.simpleMessage("Temps de jeu"),
+    "language": MessageLookupByLibrary.simpleMessage("Langue"),
+    "lets_play_together": MessageLookupByLibrary.simpleMessage(
+      "Jouons ensemble !",
+    ),
+    "loading_experience": MessageLookupByLibrary.simpleMessage(
+      "Chargement de l\'expérience...",
+    ),
+    "local_multiplayer": MessageLookupByLibrary.simpleMessage(
+      "Multijoueur local",
+    ),
+    "losses": MessageLookupByLibrary.simpleMessage("Défaites"),
+    "new_game": MessageLookupByLibrary.simpleMessage("Nouvelle partie"),
+    "no_games_yet": MessageLookupByLibrary.simpleMessage(
+      "Aucune partie jouée. Commencez à jouer pour construire votre historique !",
+    ),
+    "page_title_tic_tac_toe": MessageLookupByLibrary.simpleMessage(
+      "TIC TAC TOE",
+    ),
+    "play_vs_computer": MessageLookupByLibrary.simpleMessage(
+      "Jouer contre l\'ordinateur",
+    ),
+    "play_vs_friend": MessageLookupByLibrary.simpleMessage(
+      "Jouer contre un ami",
+    ),
+    "player_1_turn": MessageLookupByLibrary.simpleMessage("Tour du joueur 1"),
+    "player_1_wins": MessageLookupByLibrary.simpleMessage("JOUEUR 1 GAGNE !"),
+    "player_2_turn": MessageLookupByLibrary.simpleMessage("Tour du joueur 2"),
+    "player_2_wins": MessageLookupByLibrary.simpleMessage("JOUEUR 2 GAGNE !"),
+    "previous_games": MessageLookupByLibrary.simpleMessage(
+      "Parties précédentes",
+    ),
+    "restart": MessageLookupByLibrary.simpleMessage("Recommencer"),
+    "select_game_mode": MessageLookupByLibrary.simpleMessage(
+      "Sélectionner le mode de jeu",
+    ),
+    "settings": MessageLookupByLibrary.simpleMessage("Paramètres"),
+    "statistics": MessageLookupByLibrary.simpleMessage("Statistiques"),
+    "trophies": MessageLookupByLibrary.simpleMessage("Trophées"),
+    "trophies_earned": m0,
+    "ultimate_edition": MessageLookupByLibrary.simpleMessage(
+      "⚡ ÉDITION ULTIME ⚡",
+    ),
+    "victory": MessageLookupByLibrary.simpleMessage("VICTOIRE !"),
+    "vs_ai": MessageLookupByLibrary.simpleMessage("VS IA"),
+    "vs_friend": MessageLookupByLibrary.simpleMessage("VS AMI"),
+    "wins": MessageLookupByLibrary.simpleMessage("Victoires"),
+    "your_turn": MessageLookupByLibrary.simpleMessage("Votre tour"),
+  };
 }
